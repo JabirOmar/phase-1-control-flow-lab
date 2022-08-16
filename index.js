6e5a80851201e90046418faeb5e311a7eb559273
Jabir Omar <jabiromar400@gmail.com>
2022-08-16 21:11:55 +0300

    Completed

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result
+  if (distance <= 400){
+    result = 'This one is on me!'
+  }else if (distance > 2500){
+    result = 'No can do.'
+  }else if (distance > 2000){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city = "NYC"){
+  // Write your code here!  
+  return (city === "NYC") ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let reaction
+  switch (tip) {
+    case 'generous':
+      reaction = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      reaction = 'Thank you.'
+      break;
+    default:
+      reaction = 'Bye.'
+      break;
+  }
+  return reaction
 }
